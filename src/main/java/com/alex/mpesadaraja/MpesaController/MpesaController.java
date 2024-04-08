e15deb2e8bc1598154e8533335690b4d562e0c6e
Jerald kirimo <kirimojerald@gmail.com>
2024-04-08 08:42:43 +0300

    phone number and amount error check

--- src/main/java/com/alex/mpesadaraja/MpesaController/MpesaController.java
+++ src/main/java/com/alex/mpesadaraja/MpesaController/MpesaController.java
@@ -1,12 +1,12 @@
 package com.alex.mpesadaraja.MpesaController;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.mpesadaraja.dtos.sendSTKRequest;
-import com.alex.mpesadaraja.dtos.sendSTKResponse;
 import com.alex.mpesadaraja.service.DarajaApiImpl;
 
 @RestController
@@ -15,10 +15,16 @@ public class MpesaController {
     private DarajaApiImpl darajaApiImpl=new DarajaApiImpl();
 
     @GetMapping("/sendstk")
-    public sendSTKResponse sendSTK(@RequestBody sendSTKRequest request) {
+    public ResponseEntity<String> sendSTK(@RequestBody sendSTKRequest request) {
     String phoneNo=request.getPhoneNo();
     int amount=request.getAmount();
+    String regex="\\d{9}";
+    if(phoneNo!=null && amount!=0 && phoneNo.matches(regex)&& amount<=150000){
         return darajaApiImpl.sendSTK(phoneNo, amount);
+    }else{
+        System.out.println("Provided phone number and amount is invalid");
+        return null;
     }
     
+}
 }
